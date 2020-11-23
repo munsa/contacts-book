751241f8cdba8bef64f04c027304b783dd880a34
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-11-23 21:59:15 +0100

    basic page layout

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content h1').textContent).toContain('My Contacts');
+    expect(compiled.querySelector('.app-toolbar span').textContent).toContain('My Contacts');
   });
 });
