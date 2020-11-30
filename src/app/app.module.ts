cb105d54384de488a0803a56fbfef7277ccd4667
Marc Monserrat Núñez <mmonserrat90@gmail.com>
2020-11-30 02:22:15 +0100

    highlight selected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTableModule} from '@angular/material/table';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
@@ -32,7 +31,6 @@ import {ScrollingModule} from '@angular/cdk/scrolling';
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatTableModule,
     MatDialogModule,
     MatInputModule,
     MatIconModule,
